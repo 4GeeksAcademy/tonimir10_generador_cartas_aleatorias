9203f27b04f2dcef59a74f65918e99fd531e3681
Tonimir10 <91009470+Tonimir10@users.noreply.github.com>
2025-04-30 17:36:02 +0000

    cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -4,8 +4,22 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const figuraList = ['♦', '♥', '♠', '♣'];
+const numeroList = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
+const _getRandom = (array) => {
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const figuraText = _getRandom(figuraList);
+  const numeroText = _getRandom(numeroList)
+  const figuraTop = document.querySelector(".figura-top");
+  figuraTop.textContent = figuraText;
+  const figuraBottom = document.querySelector(".figura-bottom");
+  figuraBottom.textContent = figuraText;
+  const numero = document.querySelector(".number");
+  numero.textContent = numeroText;
+  
+
 };
